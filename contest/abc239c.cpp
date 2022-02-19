6214a270050e0edbe1a2729e782bac57b8fc49a3
Arjo <ac.ala.arya@gmail.com>
2022-02-19 22:37:11 +0530

    broom

---
+++ contest/abc239c.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>		// uncomment before submission
+//#include <ext/pb_ds/tree_policy.hpp>			// uncomment before submission
+//using namespace __gnu_pbds;					// uncomment before submission
+using namespace std;
+//<---------------------------------------------------Template----------------------------------------------------------->
+#define int long long
+#define ll long long
+#define ld long double
+const int INF = 1e18 + 7;
+const int MAX = 1e5 + 7;
+const int MOD = 1e9 + 7;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;                  // Vector of long long
+typedef vector<vi> vvi;                 // Vector of vi
+typedef vector<ii> vii;                 // Vector of pairs
+typedef vector<vii> vvii;               // Vector of Vector of pairs
+typedef vector<bool> vb;                // Vector of bool
+#define pq priority_queue               // Max heap (To convert to min heap, use negative sign before every value)
+#define ff first                        // For pairs
+#define ss second                       // For pairs
+#define pb push_back                    // Push back to vector
+#define mp make_pair                    // Makes pairs to be stored as pair
+#define endl "\n"                       // Changes endl to \n
+#define all(c) (c).begin(), (c).end()   // Mainly used by me in sorting
+#define range(c, r) (c).begin(), (c).begin() + (r)                // Mainly used in sorting
+#define present(c, x) ((c).find(x) != (c).end())                  // for sets, maps, multi-maps
+#define cpresent(c, x) (find(all(c),x) != (c).end())              // for vectors
+#define testcases(T) int (T); cin>>(T); while((T)--)              // inputing testcases
+#define run(x, c) for((x)=(c).begin(); (x)!=(c).end(); (x)++)     // Mainly used by me for range based loops
+// ordered_set adds two new functions to set - (set).find_by_order([kth element based on zero indexing]) and order_of_key()
+// order_of_key returns number of elements less that parameter. If element exists, that order is its index
+#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+//<----------------------------------------------------------------------------------------------------------------------->
+
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    return 0;
+}
